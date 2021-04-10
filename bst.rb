5fcb13acb0920589421259c88e93438db4b3c431
llunardon <lorenzo.lunardon.1@gmail.com>
2021-04-10 16:27:26 +0200

    added height function

--- bst.rb
+++ bst.rb
@@ -67,7 +67,7 @@ class Tree
 
     unless is_leaf?(curr)
       if curr.left != nil #if the node is't a leaf and it has a left subtree
-        substitute = max(curr.left) #substitute[0] = maximum node in the left subtree
+        substitute = max_n(curr.left) #substitute[0] = maximum node in the left subtree
         curr.value = substitute[0].value #substitute[1] = parent of substitute[0]
         substitute[1].right = nil #delete reference to the node
       else #if the node isn't a leaf but it doesn't have a left subtree
@@ -102,6 +102,61 @@ class Tree
     visited_nodes
   end
 
+  def inorder(node, visited_nodes)
+    if is_leaf?(node) #base case
+      visited_nodes.push(node.value)
+      return
+    end
+
+    inorder(node.left, visited_nodes) #recursive call on left subtree
+
+    visited_nodes.push(node.value) #visit current node
+
+    inorder(node.right, visited_nodes) #recursive call on right subtree
+
+    visited_nodes
+  end
+
+  def preorder(node, visited_nodes)
+    visited_nodes.push(node.value)
+
+    unless is_leaf?(node)
+      preorder(node.left, visited_nodes) #recursive call on left subtree
+      preorder(node.right, visited_nodes) #recursive call on right subtree
+    end
+
+    visited_nodes
+  end
+
+  def postorder(node, visited_nodes)
+    unless is_leaf?(node)
+      postorder(node.left, visited_nodes) #recursive call on left subtree
+      postorder(node.right, visited_nodes) #recursive call on right subtree
+    end
+
+    visited_nodes.push(node.value)
+
+    visited_nodes
+  end
+
+  def height(node)
+    return 0 if is_leaf?(node) #base case
+
+    if node.left != nil
+      height_left =  1 + height(node.left) 
+    else
+      height_left = 0
+    end
+
+    if node.right != nil
+      height_right =  1 + height(node.right) 
+    else
+      height_right = 0
+    end
+
+    return [height_left, height_right].max
+  end
+
   def is_leaf?(node)
     if node.left == nil && node.right == nil
       return true
@@ -125,7 +180,7 @@ def build_tree(array, first, last)
   root
 end
 
-def max(node)
+def max_n(node)
   curr = node
 
   until curr.right == nil
@@ -136,7 +191,7 @@ def max(node)
   [curr, parent]
 end
 
-def min(node)
+def min_n(node)
   curr = node
 
   until curr.left == nil
@@ -153,8 +208,11 @@ def pretty_print(node = @root, prefix = '', is_left = true)
   pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
 end
 
-arr = [4.5, 1, 69, 34, 88, 808, 2, 3, 3, 4, 5, 6, 7, 8, 11, 9]
+arr = [4.5, 1, 69, 34, 88, 1000, 808, 2, 3, 3, 4, 5, 6, 7, 8, 11, 9]
 tree = Tree.new(arr)
 pretty_print(tree.root)
 
-p tree.level_order
+#p tree.inorder(tree.root, [])
+#p tree.preorder(tree.root, [])
+#p tree.postorder(tree.root, [])
+#p tree.height(tree.find(5))
